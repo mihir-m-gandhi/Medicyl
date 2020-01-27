f2c684666b18b95f8fd3264de241ff63caa4e593
Priyam <priyam.ds@somaiya.edu>
2020-01-27 23:23:35 +0530

    none

--- medicyl/app.js
+++ medicyl/app.js
@@ -1,11 +1,24 @@
+// Add libraries here
 const express=require("express");
+const Web3=require("web3");
+const HDwalletprovider=require("truffle-hdwallet-provider");
+const bparser=require("body-parser");
+const session=require("express-session");
+const mongoStore=require("connect-mongo")(session);
+const mongoose=require('mongoose');
 
-
-const app=express();
-
+// Add routes here
 const signup=require("./controllers/signups");
 const dashboards=require("./controllers/dashboards");
 const add_record=require("./controllers/add_record");
+
+//Mongo connection
+mongoose.connect('mongodb://admin:Priyam123@ds123444.mlab.com:23444/medicyl', {useNewUrlParser: true});
+
+// Start
+const app=express();
+
+
 
 app.set('view engine','ejs');
 
