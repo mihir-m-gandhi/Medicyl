05f170ff9963bbd3997f3db63c83b175e0d0b04a
Mihir Shah <mihir.ss@somaiya.edu>
2020-01-30 00:18:08 +0530

    ddone login signup

--- medicyl/controllers/signups.js
+++ medicyl/controllers/signups.js
@@ -3,6 +3,8 @@ const fs=require("fs");
 const HDwalletprovider=require("truffle-hdwallet-provider");
 const Web3=require("web3");
 const session=require("express-session");
+const crypto = require("crypto");
+const secret = "KVNwPVFLdv9HHg9MwEgt";
 
 const createIdentity = require("./create_identity");
 
@@ -57,13 +59,13 @@ module.exports=(app)=>{
 
 
     
-    app.post("/signup",(req,res)=>{
+    app.post("/signup",async (req,res)=>{
 
         var full_name=req.body.full_name;
         var username=req.body.uname;
         var dob=req.body.dob;
         var gender=req.body.gender;
-        var pass=req.body.password;
+        var password=req.body.password;
         var cnf_pass=req.body.cnf_password;
         var contact=req.body.contact;
         var u_type="patient";
@@ -76,8 +78,6 @@ module.exports=(app)=>{
         var age = today.getFullYear() - birthDate.getFullYear();
         console.log(age);
 
-        var identity = createIdentity();
-        console.log(identity);
 
         var height="";
         var weight="";
@@ -96,15 +96,103 @@ module.exports=(app)=>{
                 console.log("Error in submission");
             }else{
                 console.log("Form Submitted Successfully");
-                return res.redirect("/dashboard")
+                
             }
 
         })
 
+        // Hash of password
+        var salt= "sRALdWPM3jqJWqN97WW1";
+        var hashpass = crypto.createHmac('sha256',secret).update(password).digest('hex');
+        console.log("Hash: ",hashpass);
+        
+
+        // Encryption of password
+        var mykey = crypto.createCipher('aes-128-cbc', salt );
+        var mystr = mykey.update(hashpass, 'utf8', 'hex');
+        mystr += mykey.final('hex');
+
+        console.log("Encryption of the hashed password",mystr);
+
+
+        //Decryption of password
+        var mykey1 = crypto.createDecipher('aes-128-cbc', salt);
+        var mystr1 = mykey1.update(mystr, 'hex', 'utf8');
+        mystr1 += mykey1.final('utf8');
+
+        console.log("Decryption of the password: ",mystr1); 
+
+
+
+
+        var identity = createIdentity();
+        console.log(identity);
 
         var newPublicKey = identity.publicKey;
         var newCompressed = ethCrypto.publicKey.compress(newPublicKey);
+
+        var newPrivateKey = identity.privateKey;
+
+        console.log(newPrivateKey)
         
+        // Converting private key to string
+        var strPKey = newPrivateKey.toString();
+
+        console.log(strPKey)
+        
+
+
+        // Encryption Code of private key
+        
+        var mypkey = crypto.createCipher('aes-128-cbc', salt );
+        
+        console.log("Private Key",typeof(newPrivateKey));
+        var mypstr = mypkey.update(strPKey, 'utf8', 'hex');
+        console.log(mypstr)
+        mypstr += mypkey.final('hex');
+
+        console.log("Encryption of Private Key",mypstr);
+
+
+        // // Decryption Code for private key 
+        var mypkey1 = crypto.createDecipher('aes-128-cbc', salt);
+        var mypstr1 = mypkey1.update(mypstr, 'hex', 'utf8');
+        mypstr1 += mypkey1.final('utf8');
+
+        mypstr1.toString(16);
+
+        console.log("Decryption of the private key: ",mypstr1); 
+
+
+        console.log(mypstr.length);
+
+
+
+        identity.compressed=newCompressed;
+        
+        // Web3 provider setup
+        const provider = new HDwalletprovider(
+            process.env.PRIVATE_KEY,
+            process.env.ROPSTEN_INFURA
+        );
+        
+        const web3=new Web3(provider);
+
+        if (web3.currentProvider !== 'undefined'){
+            console.log("Provider is set");
+        }
+        
+        const contract=new web3.eth.Contract(acc_abi,acc_address);
+
+        // Set call to contract
+        const response= await contract.methods.set(username,mypstr,mystr).send({
+             from:"0x1793A52C3B7b555f37628a0A03E5239d79353823"
+        });
+        console.log("Set Call to contract")
+        console.log(response);
+
+
+        return res.redirect("/")
 
         
     });
