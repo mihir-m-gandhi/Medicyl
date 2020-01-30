5a046cac944c3da1961e029880352d1ce6c92489
Vishal Jain <vishal.kk@somaiya.edu>
2020-01-30 10:26:03 +0530

    dsdsd

--- medicyl/controllers/doctor_doc.js
+++ medicyl/controllers/doctor_doc.js
@@ -1,12 +1,21 @@
 require("dotenv").config();
-
-var express=require('express');
+// Add libraries here
+const express=require("express");
+const Web3=require("web3");
+const HDwalletprovider=require("truffle-hdwallet-provider");
+const bparser=require("body-parser");
+const session=require("express-session");
+const mongoStore=require("connect-mongo")(session);
+const mongoose=require('mongoose');
 
 
 module.exports=(app)=>{
 
     app.get("/doctor_doc",(req,res)=>{
-        res.render("doctor_doc",{message:null});
+    	patientlist=req.session.patientlist;
+    	console.log("yes entered");
+    	console.log(patientlist);
+        res.render("doctor_doc",{patient:patientlist});
     });
 }
 
